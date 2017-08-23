1f025b27fa76ac8c320a0d0a897317ba21dea370
flyPpig <18255531271@163.com>
2017-08-23 18:28:24 +0800

    static文件，templates文件，首页视图

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,7 @@
 # coding: utf-8
 
 from django.db import models
+from django.urls import reverse
 from django.contrib.auth.models import User
 from django.utils.six import python_2_unicode_compatible
 
@@ -60,3 +61,6 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('blog:detail', kwargs={'pk': self.pk})
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+# coding: utf-8
 
-# Create your views here.
+import markdown
+from django.shortcuts import render, get_object_or_404
+from .models import *
+
+
+def index(request):
+
+    post_list = Post.objects.all().order_by('-created_time')
+    return render(request,
+                  'blog/index.html',
+                  context={
+                      'title': 'FlyPpig的博客',
+                      'post_list': post_list,
+                  })
+
+
+def detail(request, pk):
+
+    post = get_object_or_404(Post, pk=pk)
+    post.body = markdown.markdown(post.body,
+                                  extensions=[
+                                      'markdown.extensions.extra',
+                                      'markdown.extensions.codehilite',
+                                      'markdown.extensions.toc',
+                                  ])
+    return render(request, 'blog/detail.html', context={'post': post})
